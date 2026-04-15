056a3181cacef218dc509b73c3c6ef52c0f53abb
King <nguyenquocvuong12321@gmail.com>
2026-04-15 19:04:41 +0700

    update interface and image

--- script.js
+++ script.js
@@ -5,9 +5,48 @@ window.onload = function() {
     const nameDisplay = document.getElementById('guest-name');
 
     if (guestName) {
-        nameDisplay.innerText = guestName.replace(/_/g, ' ');
+        nameDisplay.innerText = guestName
     }
 
     // Hiệu ứng nhẹ khi cuộn
     console.log("Chúc mừng tốt nghiệp, Vương!");
 };
+
+
+function openGoogleMaps() {
+    window.open("https://maps.google.com?q=Đại+học+Công+Thương+TP.HCM,+140+Lê+Trọng+Tấn,+Tây+Thạnh,+TP.HCM", "_blank");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const music = document.getElementById('bg-music');
+    const musicBtn = document.getElementById('music-btn');
+    const musicIcon = document.getElementById('music-icon');
+
+    // Hàm đổi trạng thái nhạc
+    function toggleMusic() {
+        if (music.paused) {
+            music.play();
+            musicIcon.innerText = '🔊';
+            musicBtn.classList.add('rotating');
+        } else {
+            music.pause();
+            musicIcon.innerText = '🎵';
+            musicBtn.classList.remove('rotating');
+        }
+    }
+
+    // Sự kiện khi nhấn nút
+    musicBtn.addEventListener('click', (e) => {
+        e.stopPropagation(); // Ngăn sự kiện click lan ra ngoài
+        toggleMusic();
+    });
+
+    // Mẹo: Tự động phát khi khách bấm vào thiệp lần đầu (để lách luật autoplay)
+    document.body.addEventListener('click', () => {
+        if (music.paused) {
+            music.play();
+            musicIcon.innerText = '🔊';
+            musicBtn.classList.add('rotating');
+        }
+    }, { once: true }); // Chỉ chạy đúng 1 lần duy nhất
+});
